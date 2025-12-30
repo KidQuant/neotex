ec314d59f08cf5c2dc25dc73fe78473524cb0c87
KidQuant <38800128+KidQuant@users.noreply.github.com>
2025-12-30 18:18:31 -0500

    Updates
    New package for toggleterm and updates for lazy-lock.

---
+++ lua/neotex/plugins/ui/toggleterm.lua
@@ -0,0 +1,20 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		open_mapping = [[<c-\>]],
+		shade_filetypes = {},
+		hide_numbers = true,
+		shading_factor = 2,
+		direction = "float",
+		insert_mappings = true,
+		close_on_exit = true,
+		shell = vim.o.shell,
+		float_opt = {
+			border = "curved",
+		},
+	},
+	config = function(_, opts)
+		require("toggleterm").setup(opts)
+	end,
+}
